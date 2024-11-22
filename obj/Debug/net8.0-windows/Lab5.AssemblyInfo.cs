838ad61272385651e36bf5ff337d23bd45ae1a12
AnastasiaFeduniak <elkingsley1965@gmail.com>
2024-11-22 20:02:10 +0200

    Commit 3

--- obj/Debug/net8.0-windows/Lab5.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Lab5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+500d5d0e771c1381908346a0b841d636000453ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20235910f91afa3e3fbeab093a03e5363563c0d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
